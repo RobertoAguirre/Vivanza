e9dab6ab5c7faa100120a65159676406d9d02e56
roberto aguirre <robert_strife@hotmail.com>
2021-01-21 18:29:28 -0700

    changed from localhost to production

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,8 +7,8 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 export class ApiService {
 
 
-  //serviceUrl = 'http://74.208.145.99:40000/api/';
-  serviceUrl = 'http://localhost:40000/api/';
+  serviceUrl = 'http://74.208.145.99:40000/api/';
+  //serviceUrl = 'http://localhost:40000/api/';
   constructor(
     private http: HttpClient
   ) {
