9051b5687d59d5575048eb016e62372234e52db2
Luis Ríos <luis.rios@azursis.com>
2021-02-03 15:48:29 -0700

    .

--- src/app/sidebarModule/sidebar/sidebar.component.ts
+++ src/app/sidebarModule/sidebar/sidebar.component.ts
@@ -9,19 +9,9 @@ import { Router } from '@angular/router';
 })
 export class SidebarComponent implements OnInit {
 
-<<<<<<< HEAD
-  public gruposLogin;
-  public menus;
-  public id;
-  public grupoModulos;
-  public modulos;
-  public MODULOSPORGRUPO = [];
-
-=======
 public gruposLogin;
 public menus;
 public MODULOSPORGRUPO = [];
->>>>>>> 83d8591a3a3e6a41062467d84bcc6ecd8f05713d
   constructor(
     public apiService: ApiService,
     private router: Router
@@ -30,67 +20,6 @@ public MODULOSPORGRUPO = [];
   }
 
   ngOnInit(): void {
-<<<<<<< HEAD
-    this.GeneraMenu();
-  }
-
-  Filtrar(nombre_grupo) {
-    return this.modulos.filter(function (item) { return (item.nombre_grupo == nombre_grupo); });
-  }
-
-
-  GeneraMenu() {
-
-
-    let data = {
-      "appname": "VIVANZA",
-      "sp": "Trae_Modulos_Web_Usuario",
-      "params": [parseInt(this.id)]
-
-    }
-
-    this.apiService.ejecuta(data).subscribe((response) => {
-      let _response;
-      _response = response;
-
-      
-      this.menus = _response.success.recordsets[1];
-      this.modulos = _response.success.recordsets[0];
-
-      this.menus.forEach(value => {
-        this.grupoModulos = {
-          'idgrupo': value.id_grupo,
-          'nombre_grupo': value.nombre_grupo,
-          'modulos': []
-        }
-        this.modulos = _response.success.recordsets[0];
-         this.modulos = this.Filtrar(value.nombre_grupo);
-
-        this.modulos.forEach(value => {
-          this.grupoModulos['modulos'].push(value);
-        }) 
-
-        this.MODULOSPORGRUPO.push(this.grupoModulos);
-        
-
-      })
-
-      /*
-       selectChildren(data, $event) {
-          let parentChecked = data.checked;
-          this.hierarchicalData.forEach(obj => {
-              obj.forEach(childObj=> {
-                value.checked = parentChecked;
-            });
-          });
-        }
-      */
-
-    })
-  }
-
-  
-=======
     //let personal; 
     this.MODULOSPORGRUPO = JSON.parse(localStorage.getItem('MODULOSPORGRUPO'));
 
@@ -98,7 +27,6 @@ public MODULOSPORGRUPO = [];
   }
 
 
->>>>>>> 83d8591a3a3e6a41062467d84bcc6ecd8f05713d
 
 
 }
